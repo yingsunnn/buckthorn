e96797076a75a46d21d825b24b38b8705e6ac4f8
Ying <sco.suen@gmail.com>
2017-11-13 23:16:55 -0500

    feat: Update home component

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { HotEntriesComponent } from './hot-entries/hot-entries.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'hot-entries',
+        pathMatch: 'full'
+      },
+      {
+        path: 'hot-entries',
+        component: HotEntriesComponent
+      }
+      
+    ]
   }
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HotEntriesComponent } from './hot-entries/hot-entries.component';
+import { EntryCardComponent, EntryListComponent } from '../shared';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [
+    HomeComponent, 
+    HotEntriesComponent,
+    EntryCardComponent,
+    EntryListComponent
+  ]
 })
 export class HomeModule { }
